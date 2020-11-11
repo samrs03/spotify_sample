1ba3690845cfcd196e47aa19e64225eb90069d17
Samuel <samuel.rodriguez@ezlo.com>
2020-11-11 18:56:05 -0500

    The backend is now able to handle the domains specified by enviroment variables before running

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const http = require('http');
 
 const app = express();
 const tracksRoute = require('./routes/tracks').router;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.frontend_domain); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use('/tracks',tracksRoute);
 
 
--- routes/tracks.js
+++ routes/tracks.js
@@ -5,70 +5,44 @@ const config = require('config');
 const router = express.Router();
 
 router.get('/',async (req,res) => {
-    var qFlag,accessToken,limit;
+    var qFlag,accessToken,limit,offset;
     "q" in req.query ? (qFlag = true) : (qFlag = false);
     "limit" in req.query ? limit = req.query.limit : limit = 5;
+    "offset" in req.query ? offset = req.query.offset : ofsset = 0;
     if (qFlag) {
-        // params.append('grant_type','client_credentials')
-        // const options = {
-        //     baseURL: config.get('spotifyAuthorizationEndPoint'),
-        //     method: 'post',
-        //     data : 'grant_type:client_credentials',
-        //     headers : {
-        //         "Authorization" : "Basic " + functions.encodingToBase64(process.env.client_id,process.env.client_secret),
-        //         "Content-Type" : "application/x-www-form-urlencoded"
-        //     }
-        // };
-
-
-        // axios(options).then(response=> {
-        //     console.log(response.json());
-        // }).catch(error => {
-        //     console.log(error.response);
-        //     console.log("An ERror Ocurred")
-        // });
-
-
-
-
-
-
-
-
-
-
-
-
         axios.post(config.get('spotifyAuthorizationEndPoint'), 
         'grant_type=client_credentials', 
         {
         headers: {
             "Authorization" : "Basic " + functions.encodingToBase64(process.env.client_id,process.env.client_secret),
-            "Content-Type" : "application/x-www-form-urlencoded"
+            "Content-Type" : "application/x-www-form-urlencoded",
+            "Access-Control-Allow-Origin" : '*'
         }
         }).then(response => {
             accessToken = response.data.token_type + " " + response.data.access_token;
-            const options = {
+            let options = {
                 baseURL: config.get('spotifySearchSongsEndPoint'),
                 method: 'get',
                 headers: {
-                    "Authorization" : accessToken
+                    "Authorization" : accessToken,
+                    "Access-Control-Allow-Origin" : '*'
                 },
                 params: {
                     q: req.query.q,
                     limit: limit,
-                    type: 'track'
+                    type: 'track',
+                    offset: offset
                 }
             };
             axios(options).then(response => {
                 res.status(200).send(response.data);
             }).catch(error => {
                 console.log("There was an error while trying to retrieve the songs with the q specified");
-                res.status(500).send(errors.response.statusText);
+                res.status(500).send(error);
             });
         }).catch(error => {
             console.log("There was an error while trying to get the access token from server");
-            res.status(500).send(errors.response.statusText);
+            res.status(500).send(error.response.statusText);
         });
 
     } else {
